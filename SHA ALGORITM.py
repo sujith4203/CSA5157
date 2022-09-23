00eb7cfceb34859fde38edb08476c7622daa5139
B.sujith <110800611+sujith4203@users.noreply.github.com>
2022-09-23 20:33:45 +0530

    Add files via upload

---
+++ SHA ALGORITM.py
@@ -0,0 +1,6 @@
+# python3 code to check
+# available algorithms
+import hashlib
+#prints all available algorithms 
+print("the available algorithms once:",end="")
+print(hashlib.algorithms_guaranteed)
